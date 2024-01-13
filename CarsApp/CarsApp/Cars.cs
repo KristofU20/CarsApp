b02b983c6d636b2e77e9f2665426e520b06ef8ba
USER <USER@DESKTOP-93BRQDO>
2024-01-13 17:46:15 +0100

    Projekt Cars

---
+++ CarsApp/CarsApp/Cars.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace CarsApp
+{
+    public abstract class Cars
+    {
+        public Cars(string mark, string model, string bodytype, string yearofproduction)
+        {
+            this.Mark = mark;
+            this.Model = model;
+            this.BodyType = bodytype;
+            this.YearOfProduction = yearofproduction;
+        }
+
+        public string Mark { get; private set; }
+        public string Model { get; private set; }
+
+        public string BodyType { get; private set; }
+        public string YearOfProduction { get; private set; }
+
+    }
+}
+
--- CarsApp/CarsApp/Program.cs
+++ CarsApp/CarsApp/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using CarsApp;
+
+Console.WriteLine("Witaj w aplikacji CarsApp służącej do oceny aut");
+Console.WriteLine("===========================================");
+Console.WriteLine();
+
+var cars = new CarsInFile("Opel", "Corsa", "Hatchback", "2000");
+cars.GradeAdded += CarsGradeAdded;
+
+void CarsGradeAdded(object sender, EventArgs args)
+{
+    Console.WriteLine("Dodano nową ocenę");
+}
+
+
+while (true)
+{
+    Console.WriteLine("Podaj kolejną  ocenę auta: ");
+    var input = Console.ReadLine();
+    if (input == "q")
+    {
+        break;
+    }
+
+    try
+    {
+        cars.AddGrade(input);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine($"Exception catched: {e.Message}");
+    }
+}
+
+var statistics = cars.GetStatistics();
+Console.WriteLine($"Average: {statistics.Average}");
+Console.WriteLine($"Min:  {statistics.Min}");
+Console.WriteLine($"Max:  {statistics.Max}");
+Console.WriteLine($"AvarageLetter: {statistics.AverageLetter}");
